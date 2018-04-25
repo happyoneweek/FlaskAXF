742f159679aa4d6efd19df537cae3d28b45bc1a7
xuguoliang1995 <1257646768@qq.com>
2018-04-25 15:40:50 +0800

    第一次修改完之後

--- App/models.py
+++ App/models.py
@@ -1,6 +1,5 @@
 from App.exts import db
 
-
 class HomeBanner(db.Model):
     __tablename__='axf_wheel'
     id=db.Column(db.Integer,primary_key=True,autoincrement=True)
@@ -14,3 +13,13 @@ class GoodType(db.Model):
     typename=db.Column(db.String(200))
     childtypename=db.Column(db.String(200))
     typesort=db.Column(db.Integer,default=1)
+
+class UserModel(db.Model):
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    username = db.Column(db.String(32), unique=True)
+    password = db.Column(db.String(256))
+    email = db.Column(db.String(64), unique=True)
+    sex = db.Column(db.Boolean(), default=False)
+    icon = db.Column(db.String(256))
+    is_delete = db.Column(db.Boolean(), default=False)
+
